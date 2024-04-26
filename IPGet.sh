4959ddde5bd51a5a440349480795d5f88ce14a7e
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2024-04-26 15:32:44 +0800

    Update IPGet.sh

--- IPGet.sh
+++ IPGet.sh
@@ -4,4 +4,4 @@ apt install -y jq
 
 jq '.values[] | select((.region | tostring) | startswith("ap") and any(.name[]; . == "HongKong" or . == "Singapore") | not) | .properties.addressPrefixes' ServiceTags_Public.json | grep -v : | sed -e 's/"//g;s/,//g;s/\[//g;s/\]//g;s/{//g;s/}//g' | grep / > ipv4.txt
 
-
+jq '.values[] | select((.region | tostring) | startswith("ap") and any(.name[]; . == "HongKong" or . == "Singapore") | not) | .properties.addressPrefixes' ServiceTags_Public.json | grep : | sed -e 's/"//g;s/,//g;s/\[//g;s/\]//g;s/{//g;s/}//g'

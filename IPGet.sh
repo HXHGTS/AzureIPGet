289c77d285010af8f80f04578c5ba738c4373019
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2024-04-26 15:49:45 +0800

    Update IPGet.sh

--- IPGet.sh
+++ IPGet.sh
@@ -2,6 +2,15 @@
 
 apt install -y jq
 
-jq '.values[] | select((.region | tostring) | startswith("ap") and any(.name[]; . == "HongKong" or . == "Singapore") | not) | .properties.addressPrefixes' ServiceTags_Public.json | grep -v : | sed -e 's/"//g;s/,//g;s/\[//g;s/\]//g;s/{//g;s/}//g;s/ //g' | grep / > ipv4.txt
+jq '.values[] | select((.region | tostring) | startswith("ap") and any(.name[]; . == "HongKong" or . == "Singapore") | not) | .properties.addressPrefixes' ServiceTags_Public.json | grep -v : | sed -e 's/"//g;s/,//g;s/\[//g;s/\]//g;s/{//g;s/}//g;s/ //g' | grep / > ip.txt
 
-jq '.values[] | select((.region | tostring) | startswith("ap") and any(.name[]; . == "HongKong" or . == "Singapore") | not) | .properties.addressPrefixes' ServiceTags_Public.json | grep : | sed -e 's/"//g;s/,//g;s/\[//g;s/\]//g;s/{//g;s/}//g;s/ //g' | grep / > ipv6.txt
+jq '.values[] | select((.region | tostring) | startswith("ap") and any(.name[]; . == "HongKong" or . == "Singapore") | not) | .properties.addressPrefixes' ServiceTags_Public.json | grep : | sed -e 's/"//g;s/,//g;s/\[//g;s/\]//g;s/{//g;s/}//g;s/ //g' | grep / >> ip.txt
+
+# 将ip.txt文件中的每行读取为JSON数组
+ip_cidr=$(jq -R -s 'split("\n")[:-1]' ip.txt)
+
+# 使用jq生成JSON结构
+echo "{\"route\": {\"rules\": [{\"ip_cidr\": $ip_cidr, \"outbound\": \"REJECT\"}]}}" > ip.json
+
+# 输出最终的ip.json文件内容
+cat ip.json

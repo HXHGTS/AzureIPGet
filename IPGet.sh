3606f44f1578c20240938d39605339a3c6f0619a
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2024-05-02 18:36:08 +0800

    Update IPGet.sh

--- IPGet.sh
+++ IPGet.sh
@@ -10,12 +10,32 @@ jq '.values[] | select(.properties.region | test("japaneast|japanwest|koreacentr
 
 #jq '.values[] | select(.properties.region | test("japaneast|japanwest|koreacentral|koreasouth")) | select(.properties.systemService | test("sql")) | .properties.addressPrefixes' ServiceTags_Public.json | grep : | sed -e 's/"//g;s/,//g;s/\[//g;s/\]//g;s/{//g;s/}//g;s/ //g' | grep / >> ip.txt
 
-wget -O ip2json.sh  https://raw.githubusercontent.com/HXHGTS/AzureIPGet/main/ip2json.sh
+# 检查 /usr/local/etc/xray 目录是否存在
+if [ -d "/usr/local/etc/xray" ]; then
+    # 下载 ip2xray.sh 脚本
+    wget -O ip2xray.sh https://raw.githubusercontent.com/HXHGTS/AzureIPGet/main/ip2xray.sh
+    # 赋予执行权限
+    chmod +x ip2xray.sh
+    # 执行脚本并输出到 REJECT.json
+    bash ip2xray.sh > /usr/local/etc/xray/REJECT.json
+    # 输出 REJECT.json 的内容
+    cat /usr/local/etc/xray/REJECT.json
+    # 重启 xray 服务
+    systemctl restart xray
+# 如果 /usr/local/etc/xray 目录不存在，检查 /etc/sing-box 目录是否存在
+elif [ -d "/etc/sing-box" ]; then
+    # 下载 ip2sing.sh 脚本
+    wget -O ip2sing.sh https://raw.githubusercontent.com/HXHGTS/AzureIPGet/main/ip2sing.sh
+    # 赋予执行权限
+    chmod +x ip2sing.sh
+    # 执行脚本并输出到 REJECT.json
+    bash ip2sing.sh > /etc/sing-box/REJECT.json
+    # 输出 REJECT.json 的内容
+    cat /etc/sing-box/REJECT.json
+    # 重启 sing-box 服务
+    systemctl restart sing-box
+# 如果两个目录都不存在，返回提示信息
+else
+    echo "未发现可执行脚本的路径！"
+fi
 
-chmod +x ip2json.sh
-
-bash ip2json.sh > /etc/sing-box/REJECT.json
-
-cat /etc/sing-box/REJECT.json
-
-systemctl restart sing-box

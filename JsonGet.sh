2c3f81b318c3e727e50dbcf4068b45df4cac74e2
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2024-05-15 19:36:52 +0800

    Update JsonGet.sh

--- JsonGet.sh
+++ JsonGet.sh
@@ -2,7 +2,7 @@
 
 apt install -y jq wget
 
-wget -O ServiceTags_Public.json https://raw.githubusercontent.com/HXHGTS/AzureIPGet/main/ServiceTags_Public.json
+curl -fsSL https://raw.githubusercontent.com/HXHGTS/AzureIPGet/main/IPGet.sh | bash
 
 jq '.values[] | select(.id | test("AzureCloud")) | select(.properties.region | test("japaneast|japanwest|koreacentral|koreasouth")) | .properties.addressPrefixes' ServiceTags_Public.json | grep -v : | sed -e 's/"//g;s/,//g;s/\[//g;s/\]//g;s/{//g;s/}//g;s/ //g' | grep / >ip.txt
 

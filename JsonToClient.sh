4c235068d8db9259e5e2fac42d01fc65493f1bde
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2024-05-28 07:34:48 +0800

    Create JsonToClient.sh

---
+++ JsonToClient.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+apt install -y jq wget sed coreutils
+
+curl -fsSL https://raw.githubusercontent.com/HXHGTS/AzureIPGet/main/IPGet.sh | bash
+
+jq '.values[] | select(.id | test("AzureCloud")) | select(.properties.region | test("japaneast|japanwest|koreacentral|koreasouth")) | .properties.addressPrefixes' ServiceTags_Public.json | grep -v : | sed -e 's/"//g;s/,//g;s/\[//g;s/\]//g;s/{//g;s/}//g;s/ //g' | grep / >ip.txt
+
+#wget -O ip2xray.sh https://raw.githubusercontent.com/HXHGTS/AzureIPGet/main/ip2xray.sh
+
+#chmod +x ip2xray.sh
+
+#bash ip2xray.sh >/usr/local/etc/xray/REJECT.json
+
+#cat REJECT.json
+
+#rm -f ip2xray.sh ip.txt
+
+wget -O ip2sing.sh https://raw.githubusercontent.com/HXHGTS/AzureIPGet/main/ip2sing.sh
+
+chmod +x ip2sing.sh
+
+bash ip2sing.sh REJECT.json
+
+cat REJECT.json
+
+rm -f ip2sing.sh ip.txt ip.txt ServiceTags_Public.json

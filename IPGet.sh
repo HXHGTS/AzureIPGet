f9b7ae743439477830556dec942b8197a646a1f3
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2026-01-30 17:56:08 +0800

    Update IPGet.sh

--- IPGet.sh
+++ IPGet.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+UA='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36'
+
 echo '正在获取AzureIPRange数据库下载地址. . .'
 
-curl 'https://www.microsoft.com/en-us/download/details.aspx?id=56519' --user-agent 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36' | sed -e 's/"/\n/g' | grep ServiceTags_Public_ | grep https | head -n 1 >GetAzureIPRange.txt
+curl 'https://www.microsoft.com/en-us/download/details.aspx?id=56519' --user-agent ${UA} | sed -e 's/"/\n/g' | grep ServiceTags_Public_ | grep https | head -n 1 >GetAzureIPRange.txt
 
 echo '正在下载AzureIPRange数据库. . .'
 
